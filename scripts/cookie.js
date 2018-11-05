04b4d2216b28c46b1986f127661771fe74869396
Shane Lucy <44270630+ShaneLucy@users.noreply.github.com>
2018-11-05 13:42:56 +0000

    Create cookie.js

---
+++ scripts/cookie.js
@@ -0,0 +1,13 @@
+window.addEventListener("load", function(){
+window.cookieconsent.initialise({
+  "palette": {
+    "popup": {
+      "background": "#000"
+    },
+    "button": {
+      "background": "#f1d600"
+    }
+  },
+  "theme": "classic",
+  "position": "top"
+})});
