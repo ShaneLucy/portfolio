23400a022adc3472874833615d81fde1064c1752
Shane Lucy <44270630+ShaneLucy@users.noreply.github.com>
2018-12-08 11:56:34 +0000

    Update tooltip.js

--- scripts/tooltip.js
+++ scripts/tooltip.js
@@ -1,5 +1,31 @@
-//applies bootstrap css to tooltips
-$(function() {
-    $('[data-toggle="tooltip"]').tooltip();
+$(function () {
+	//applies bootstrap css to tooltips
+	$('[data-toggle="tooltip"]').tooltip();
+
+
+	//toggles visibilty off card headings and content on mouseover and mouseleave 
+	$("#card-img-1").mouseover(function () {
+		$('.card-1').css('display', 'block');
+		$('#card-img-2').css('display', 'none');
+	});
+
+
+	$("#card-img-1").mouseout(function () {
+		$('.card-1').css('display', 'none');
+		$('#card-img-2').css('display', 'block');
+	});
+
+	$("#card-img-2").mouseover(function () {
+		$('.card-2').css('display', 'block');
+		$('#card-img-1').css('display', 'none');
+
+	});
+
+
+	$("#card-img-2").mouseout(function () {
+		$('.card-2').css('display', 'none');
+		$('#card-img-1').css('display', 'block');
+	});
+
 
 });
