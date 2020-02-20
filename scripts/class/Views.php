d53077a29dd9cc620c6762541af758c60f206dc4
Shane Lucy <44270630+ShaneLucy@users.noreply.github.com>
2020-02-20 13:30:16 +0000

    Improved Logic
    I improved the logic in counting the page views by distinguishing between total views, annual views and monthly views

--- scripts/class/Views.php
+++ scripts/class/Views.php
@@ -3,7 +3,9 @@ class Views {
     private $viewCount;
     protected $conn;
     protected $selectViews;
-    protected $updateViews;
+    protected $updateMonthlyViews;
+    protected $updateAnnualViews;
+    protected $updateTotalViews;
 
     public function __construct() {
         //when the website is visited set the view count to 1
@@ -11,7 +13,7 @@ class Views {
     }
 
     public function dbConnection() {
-        include_once "database_credentials";
+        include_once "credentials";
         try {
             $this->conn = new PDO($dsn, $user, $password);
         } catch (PDOException $e) {
@@ -21,11 +23,15 @@ class Views {
 
     public function prepareStatements() {
         //creating prepared statements
-        $this->selectViews = $this->conn->prepare(
-            "SELECT views FROM PageViews"
+        $this->selectViews = $this->conn->prepare("SELECT * FROM PageViews");
+        $this->updateMonthlyViews = $this->conn->prepare(
+            "UPDATE PageViews SET monthlyViews=? WHERE ID =?"
         );
-        $this->updateViews = $this->conn->prepare(
-            "UPDATE PageViews SET views=? WHERE ID =?"
+        $this->updateAnnualViews = $this->conn->prepare(
+            "UPDATE PageViews SET annualViews=? WHERE ID =?"
+        );
+        $this->updateTotalViews = $this->conn->prepare(
+            "UPDATE PageViews SET totalViews=? WHERE ID =?"
         );
     }
 
@@ -33,23 +39,42 @@ class Views {
         //execute select * query
         $this->selectViews->execute();
         $row = $this->selectViews->fetch();
-        //get the total views prior to the current page view
-        $views = $row[0];
+        //assign monthly, annual and total views to variables
+        $totalViews = $row[1];
+        $annualViews = $row[2];
+        $monthlyViews = $row[3];
 
-        if ($views > 0) {
-            //if the views are greated than 0 add the current page view to the number of views returned from the db
-            $totalViews = $views + $this->viewCount;
-            //update the db with the total views
-            $this->updateViews->execute([$totalViews, 1]);
+        //if the monthly views are greater than 0 add the current page view to the number of monthly views returned from the db
+        if ($monthlyViews > 0) {
+            $updatedMonthlyViews = $monthlyViews + $this->viewCount;
+            //update the db with the total monthly views
+            $this->updateMonthlyViews->execute([$updatedMonthlyViews, 1]);
         } else {
             //otherwise insert the current view into the db
-            $this->updateViews->execute([$this->viewCount, 1]);
+            $this->updateMonthlyViews->execute([$this->viewCount, 1]);
+        }
+
+        //repeat the above statement for annual and total views
+        if ($annualViews > 0) {
+            $updatedAnnualViews = $annualViews + $this->viewCount;
+            $this->updateAnnualViews->execute([$updatedAnnualViews, 1]);
+        } else {
+            $this->updateAnnualViews->execute([$this->viewCount, 1]);
+        }
+
+        if ($totalViews > 0) {
+            $updatedTotalViews = $totalViews + $this->viewCount;
+            $this->updateTotalViews->execute([$updatedTotalViews, 1]);
+        } else {
+            $this->updateTotalViews->execute([$this->viewCount, 1]);
         }
     }
 
     public function terminateConnections() {
         $this->conn = null;
         $this->selectViews = null;
-        $this->updateViews = null;
+        $this->updateMonthlyViews = null;
+        $this->updateAnnualViews = null;
+        $this->updateTotalViews = null;
     }
 }
