4d5cee1c704bbde95b65a9d90b8e89f9b99a7a7c
Shane Lucy <44270630+ShaneLucy@users.noreply.github.com>
2020-02-20 13:33:10 +0000

    Improved Logic
    Previously I was emailing the total views on a monthly basis. Now the correct views are emailed monthly and then the monthly views will be reset

--- scripts/class/MonthlyEmail.php
+++ scripts/class/MonthlyEmail.php
@@ -1,23 +1,25 @@
 <?php
 include_once "Views.php";
-class Cron extends Views {
+class MonthlyEmail extends Views {
     public function emailViews() {
         //get the total page views from the db
         $this->selectViews->execute();
         $row = $this->selectViews->fetch();
-        $views = $row[0];
+        $monthlyViews = $row[3];
 
-        //construct a message to be emailed
+        //construct the message to be emailed
         $message =
             "Over the past month there has been " .
-            $views .
+            $monthlyViews .
             " page views on shanelucy.me";
         //call the mail function
         mail('email@address', 'shanelucy.me Page Views', $message);
+        //finally reset the monthly views to 0
+        $this->updateMonthlyViews->execute([0, 1]);
     }
 }
 
-$cron = new Cron();
+$cron = new MonthlyEmail();
 $cron->dbConnection();
 $cron->prepareStatements();
 $cron->emailViews();
