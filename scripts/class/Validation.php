e472537f1c8ea56ef191b4f5b4605b5b8be5e239
Shane Lucy <44270630+ShaneLucy@users.noreply.github.com>
2020-03-04 19:08:35 +0000

    Create Validation.php

---
+++ scripts/class/Validation.php
@@ -0,0 +1,60 @@
+<?php
+session_start();
+class Validation
+{
+    protected $name;
+    protected $email;
+    protected $enquiry;
+    protected $csrfToken;
+    protected $honeypot;
+
+    public function validateName()
+    {
+        //sanitising input
+        $trimmed = trim($_POST["name"]);
+        $sanitised = filter_var($trimmed, FILTER_SANITIZE_STRING);
+        $name = htmlspecialchars($sanitised);
+        //name may only contain letters and spaces
+        if (preg_match("/[A-Za-z ]/", $name) && strlen($name) >= 3) {
+            $this->name = $name;
+        }
+    }
+
+    public function validateEmail()
+    {
+        $trimmed = trim($_POST["email"]);
+        if (filter_var($trimmed, FILTER_VALIDATE_EMAIL)) {
+            $this->email = $trimmed;
+        }
+    }
+
+    public function validateEnquiry()
+    {
+        $trimmed = trim($_POST["enquiry"]);
+        $sanitised = strip_tags($trimmed);
+        $enquiry = htmlspecialchars($sanitised);
+        //only set the enquiry if it contains 20 chars or more
+        if (strlen($enquiry) >= 20) {
+            $this->enquiry = $enquiry;
+        }
+    }
+
+    public function verifyCsrfToken()
+    {
+        $token = $_POST["token"];
+        //echo $_SESSION['csrfToken']; not working in class scope
+        if ($_SESSION['csrfToken'] == $token) {
+            $this->csrfToken = $token;
+        }
+    }
+
+    public function honeypot()
+    {
+        //a basic method to help prevent spam bots from filling in the form
+        $number = $_POST["mobile-number"];
+        //if the value is changed the validation fails and the value won't be set
+        if ($number == 0) {
+            $this->honeypot = 0;
+        }
+    }
+}
