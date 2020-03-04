2e4a130667ceb10cbbe56d2384b23ffdf3e4a2ee
Shane Lucy <44270630+ShaneLucy@users.noreply.github.com>
2020-03-04 19:10:27 +0000

    Create EnquiryEmail.php

---
+++ scripts/class/EnquiryEmail.php
@@ -0,0 +1,34 @@
+<?php
+include_once "Validation.php";
+
+class EnquiryEmail extends Validation
+{
+    public function email()
+    {
+        //if all fields are set, the values passed validation
+        //so construct the email and send it
+
+        if (
+            isset($this->name) &&
+            isset($this->enquiry) &&
+            isset($this->email) &&
+            isset($this->csrfToken) &&
+            isset($this->honeypot)
+        ) {
+            $message = $this->enquiry . "\r\n Reply to " . $this->email;
+            mail(
+                'shanelucy01@protonmail.com',
+                $this->name . " Web Developer Request",
+                $message
+            );
+        }
+    }
+}
+
+$newEnquiry = new EnquiryEmail();
+$newEnquiry->validateName();
+$newEnquiry->validateEmail();
+$newEnquiry->validateEnquiry();
+$newEnquiry->verifyCsrfToken();
+$newEnquiry->honeypot();
+$newEnquiry->email();
