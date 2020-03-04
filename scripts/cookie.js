999f30736a960b9c730b919c95721dff512f89c6
Shane Lucy <44270630+ShaneLucy@users.noreply.github.com>
2020-03-04 18:55:40 +0000

    Update cookie.js

--- scripts/cookie.js
+++ scripts/cookie.js
@@ -1,13 +1,19 @@
-window.addEventListener("load", function(){
-window.cookieconsent.initialise({
-  "palette": {
-    "popup": {
-      "background": "#000"
-    },
-    "button": {
-      "background": "#f1d600"
-    }
-  },
-  "theme": "classic",
-  "position": "top"
-})});
+const cookieContainer = document.getElementById("cookie-consent");
+const cookieDismiss = document.getElementById("cookie-dismiss");
+var accepted = localStorage.getItem("cookieAccepted");
+
+
+//if cookies are accepted dont display cookie consent
+if (accepted == "true") {
+    cookieContainer.style.display = "none";
+}
+
+
+cookieDismiss.addEventListener("click", dismiss);
+
+function dismiss() {
+    //set cookie that confirms the cookie display has been dismissed
+    localStorage.setItem("cookieAccepted", "true");
+    //hide the cookie container
+    cookieContainer.style.display = "none";
+}
