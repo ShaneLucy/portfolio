ae55b72e169a0a6781c898d5e9013efc83f0ca7d
Shane Lucy <44270630+ShaneLucy@users.noreply.github.com>
2020-04-02 19:47:46 +0000

    Added pepipost api to handle outgoing emails

--- scripts/class/EnquiryEmail.php
+++ scripts/class/EnquiryEmail.php
@@ -6,8 +6,6 @@ class EnquiryEmail extends Validation
     public function email()
     {
         //if all fields are set, the values passed validation
-        //so construct the email and send it
-
         if (
             isset($this->name) &&
             isset($this->enquiry) &&
@@ -15,12 +13,32 @@ class EnquiryEmail extends Validation
             isset($this->csrfToken) &&
             isset($this->honeypot)
         ) {
-            $message = $this->enquiry . "\r\n Reply to " . $this->email;
-            mail(
-                'email@mail.com',
-                $this->name . " Web Developer Request",
-                $message
-            );
+            //initialise cURL and send email using pepipost api
+            $curl = curl_init();
+            curl_setopt_array($curl, [
+                CURLOPT_URL => "https://api.pepipost.com/v2/sendEmail",
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => "",
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 30,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => "POST",
+                CURLOPT_POSTFIELDS => "{\"personalizations\":[{\"recipient\":\"me@myemail\"}],\"from\":{\"fromEmail\":\"info@mail.shanelucy.me\",\"fromName\":\"'$this->name'\"},\"subject\":\"Web Developer Request - '$this->email'\",\"content\":\"'$this->enquiry'\"}",
+                CURLOPT_HTTPHEADER => [
+                    "api_key: Key",
+                    "content-type: application/json",
+                ],
+            ]);
+
+            $response = curl_exec($curl);
+            $err = curl_error($curl);
+            curl_close($curl);
+
+            if ($err) {
+                echo "cURL Error #:" . $err;
+            } else {
+                echo $response;
+            }
         }
     }
 }
