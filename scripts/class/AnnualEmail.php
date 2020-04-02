d6b9f65e7fde902ff619f1ccf4edac28d22ce84a
Shane Lucy <44270630+ShaneLucy@users.noreply.github.com>
2020-04-02 20:05:00 +0000

    Added pepipost api to handle email

--- scripts/class/AnnualEmail.php
+++ scripts/class/AnnualEmail.php
@@ -1,7 +1,9 @@
 <?php
 include_once "Views.php";
-class AnnualEmail extends Views {
-    public function emailViews() {
+class AnnualEmail extends Views
+{
+    public function emailViews()
+    {
         //get the total page views from the db
         $this->selectViews->execute();
         $row = $this->selectViews->fetch();
@@ -12,12 +14,37 @@ class AnnualEmail extends Views {
         $message =
             "Over the past year there has been " .
             $annualViews .
-            " page views on shanelucy.me and the total views for this website to date are" .
+            " page views on shanelucy.me and the total views for this website to date are " .
             $totalViews;
-        //call the mail function
-        mail('email@address', 'shanelucy.me Page Views', $message);
-        //finally reset the annual views to 0
-        $this->updateAnnualViews->execute([0, 1]);
+        //send the request to pepipost api
+        $curl = curl_init();
+        curl_setopt_array($curl, [
+            CURLOPT_URL => "https://api.pepipost.com/v2/sendEmail",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_POSTFIELDS => "{\"personalizations\":[{\"recipient\":\"myemail@address.com\"}],\"from\":{\"fromEmail\":\"info@mail.shanelucy.me\",\"fromName\":\"\"},\"subject\":\"Annual Page Views\",\"content\":\"'$message'\"}",
+            CURLOPT_HTTPHEADER => [
+                "api_key: Key",
+                "content-type: application/json",
+            ],
+        ]);
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+        if ($err) {
+            echo "cURL Error #:" . $err;
+        } else {
+            echo $response;
+            //finally reset the annual views to 0
+            $this->updateAnnualViews->execute([0, 1]);
+        }
     }
 }
 
