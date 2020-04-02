5c49893c56b42319930431bf9602a29e23121079
Shane Lucy <44270630+ShaneLucy@users.noreply.github.com>
2020-04-02 19:58:10 +0000

    Added pepipost api to handle email

--- scripts/class/MonthlyEmail.php
+++ scripts/class/MonthlyEmail.php
@@ -1,7 +1,9 @@
 <?php
 include_once "Views.php";
-class MonthlyEmail extends Views {
-    public function emailViews() {
+class MonthlyEmail extends Views
+{
+    public function emailViews()
+    {
         //get the total page views from the db
         $this->selectViews->execute();
         $row = $this->selectViews->fetch();
@@ -13,9 +15,35 @@ class MonthlyEmail extends Views {
             $monthlyViews .
             " page views on shanelucy.me";
         //call the mail function
-        mail('email@address', 'shanelucy.me Page Views', $message);
-        //finally reset the monthly views to 0
-        $this->updateMonthlyViews->execute([0, 1]);
+
+        $curl = curl_init();
+        curl_setopt_array($curl, [
+            CURLOPT_URL => "https://api.pepipost.com/v2/sendEmail",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_POSTFIELDS => "{\"personalizations\":[{\"recipient\":\"myemail@address.com\"}],\"from\":{\"fromEmail\":\"info@mail.shanelucy.me\",\"fromName\":\"\"},\"subject\":\"Monthly Page Views\",\"content\":\"'$message'\"}",
+            CURLOPT_HTTPHEADER => [
+                "api_key: Key",
+                "content-type: application/json",
+            ],
+        ]);
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+        if ($err) {
+            echo "cURL Error #:" . $err;
+        } else {
+            echo $response;
+            //finally reset the monthly views to 0
+            $this->updateMonthlyViews->execute([0, 1]);
+        }
     }
 }
 
