79dbd7337720503bba83c323a2b538344f5ed478
Shane Lucy <shanelucy01@protonmail.com>
2021-10-02 19:25:15 +0100

    Started working on side menu

---
+++ src/components/desktop/components/SideMenu.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+  const menuItems = [
+    {
+      href: 'https://github.com/shanelucy/',
+      src: 'assets/github.png'
+    }
+  ];
+</script>
+
+<aside>
+  <nav>
+    {#each menuItems as menuItem}
+      <a href={menuItem.href} target="_blank" rel="noopener">
+        <img src={menuItem.src} alt="" />
+      </a>
+    {/each}
+  </nav>
+</aside>
+
+<style>
+  aside {
+    height: 100%;
+    width: max-content;
+    top: 1.95rem;
+    left: 0;
+    background-color: rgba(40, 40, 40, 0.5);
+    overflow: hidden;
+  }
+
+  nav {
+    margin: 0.75rem 1rem;
+    display: flex;
+    flex-direction: column;
+  }
+
+  img {
+    height: auto;
+    width: 3.5rem;
+    cursor: pointer;
+  }
+</style>
