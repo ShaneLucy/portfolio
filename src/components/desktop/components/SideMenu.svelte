8627f0807cdab5faa426309f5592cc67b1a2d5c9
Shane Lucy <shanelucy01@protonmail.com>
2021-10-04 14:53:15 +0100

    Added linkedin & dialog

--- src/components/desktop/components/SideMenu.svelte
+++ src/components/desktop/components/SideMenu.svelte
@@ -1,18 +1,46 @@
 <script lang="ts">
-  const menuItems = [
+  type MenuItems = {
+    href: string;
+    src: string;
+    name: string;
+  };
+
+  const menuItems: Array<MenuItems> = [
     {
       href: 'https://github.com/shanelucy/',
-      src: 'build/images/github.png'
+      src: 'build/images/github.png',
+      name: 'github'
+    },
+    {
+      href: 'https://uk.linkedin.com/in/shane-lucy-4735b616a',
+      src: 'build/images/linkedin.png',
+      name: 'linkedin'
+    },
+    {
+      href: 'dialog',
+      src: 'build/images/folder.png',
+      name: 'about me'
     }
   ];
+
+  const openFileExplorer = (): void => {
+    
+  }
 </script>
 
 <aside>
   <nav>
     {#each menuItems as menuItem}
-      <a href={menuItem.href} target="_blank" rel="noopener">
-        <img src={menuItem.src} alt="" />
+    {#if menuItem.href === 'dialog'}
+    <a href={menuItem.href} on:click|preventDefault={openFileExplorer} >
+        <img src={menuItem.src} alt={menuItem.name} />
       </a>
+      {:else}
+      <a href={menuItem.href} target="_blank" rel="noopener">
+        <img src={menuItem.src} alt={menuItem.name} />
+      </a>
+    {/if}
+      
     {/each}
   </nav>
 </aside>
@@ -28,14 +56,16 @@
   }
 
   nav {
-    margin: 0.75rem 1rem;
+    margin: 2.5rem 1.25rem;
     display: flex;
     flex-direction: column;
+    row-gap: 2.5rem;
   }
 
   img {
-    height: auto;
+    height: 3.5rem;
     width: 3.5rem;
     cursor: pointer;
+    object-fit: contain;
   }
 </style>
