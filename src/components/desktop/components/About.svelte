a5ca72d38919177193ab4881f0e2f378a1b7f3fd
Shane Lucy <shanelucy01@protonmail.com>
2021-10-05 19:25:34 +0100

    Added content to about me tab

--- src/components/desktop/components/About.svelte
+++ src/components/desktop/components/About.svelte
@@ -1,1 +1,43 @@
-About
+<script lang="ts">
+  const menuItems = [
+    {
+      name: 'about-me.txt',
+      src: 'build/images/txt.png'
+    },
+    {
+      name: 'about-me.json',
+      src: 'build/images/json.png'
+    }
+  ];
+</script>
+
+<nav>
+  {#each menuItems as menuItem}
+    <div>
+      <a href={menuItem.name} on:click|preventDefault>
+        <img src={menuItem.src} alt={menuItem.name} />
+      </a>
+      <p>{menuItem.name}</p>
+    </div>
+  {/each}
+</nav>
+
+<style>
+  nav {
+    display: flex;
+    column-gap: 1rem;
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+    row-gap: 0.5rem;
+    align-items: center;
+    padding: 0.5rem;
+    cursor: pointer;
+  }
+
+  div:hover {
+    background-color: var(--background-quaternary);
+  }
+</style>
