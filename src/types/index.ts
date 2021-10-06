8afc30b885778cafad63dbc5350dda1725bec2a8
Shane Lucy <shanelucy01@protonmail.com>
2021-10-06 19:41:52 +0100

    Created directory for types

---
+++ src/types/index.ts
@@ -0,0 +1,17 @@
+import type FileExplorer from '../components/desktop/components/dialog/FileExplorer.svelte';
+import type About from '../components/desktop/components/About.svelte';
+import type Projects from '../components/desktop/components/Projects.svelte';
+import type Home from '../components/desktop/components/Home.svelte';
+
+export interface FileExplorerMenu {
+  name: string;
+  active: boolean;
+  component: typeof Projects | About | Home;
+}
+
+export interface Dialog {
+  component: typeof FileExplorer;
+  openingActiveTab: number;
+  display: 'min' | 'default' | 'max';
+  fileExplorerState: Array<FileExplorerMenu>;
+}
