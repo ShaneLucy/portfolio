441d531d6826e1b1c1400337e8c8453ed98f62c6
Shane Lucy <shanelucy01@protonmail.com>
2021-10-12 20:37:23 +0100

    Variable changes

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -4,6 +4,6 @@ import type { Dialog } from '../types';
  * Returns the index of the active dialog
  */
 export default (dialog: Array<Dialog>): number => {
-  const activeIndex = dialog.findIndex((x) => x.isActive);
+  const activeIndex = dialog.findIndex((x) => x.active);
   return activeIndex;
 };
