61fca901f285131cd68d16060921e248eb62cf40
Shane Lucy <shanelucy01@protonmail.com>
2021-10-12 20:18:05 +0100

    Updated about tab to load correct about me dialog

--- src/components/desktop/components/About.svelte
+++ src/components/desktop/components/About.svelte
@@ -1,8 +1,19 @@
 <script lang="ts">
+  import dialogState from '../../../state';
+  import findActive from '../../../helpers';
+  import type { Dialog } from '../../../types';
+  import AboutMeTXT from './about-me/AboutMeTXT.svelte';
+  import AboutMeJSON from './about-me/AboutMeJSON.svelte';
+  import AboutMeYAML from './about-me/AboutMeYAML.svelte';
+
   const menuItems = [
     {
       name: 'about-me.txt',
       src: 'build/images/txt.png'
+    },
+    {
+      name: 'about-me.yml',
+      src: 'build/images/yaml.png'
     },
     {
       name: 'about-me.json',
@@ -10,15 +21,48 @@
     }
   ];
 
-  const openNewDialog = (): void => {
-    // do something
+  const setComponent = (index: number): any => {
+    let component;
+    if (index === 0) {
+      component = AboutMeTXT;
+    }
+
+    if (index === 1) {
+      component = AboutMeYAML;
+    }
+
+    if (index === 2) {
+      component = AboutMeJSON;
+    }
+
+    return component;
+  };
+
+  const openNewDialog = (index: number, name: string): void => {
+    if (findActive($dialogState) !== -1) {
+      $dialogState[findActive($dialogState)].active = false;
+    }
+
+    const nextState: Array<Dialog> = [
+      {
+        component: setComponent(index),
+        openingActiveTab: index,
+        display: 'default',
+        fileExplorerState: [],
+        active: true,
+        open: true,
+        title: name
+      }
+    ];
+
+    dialogState.update((val) => [...val, ...nextState]);
   };
 </script>
 
 <nav>
-  {#each menuItems as menuItem}
-    <div>
-      <a href={menuItem.name} on:click|preventDefault={openNewDialog}>
+  {#each menuItems as menuItem, index}
+    <div on:click={() => openNewDialog(index, menuItem.name)}>
+      <a href={menuItem.name} on:click|preventDefault>
         <img src={menuItem.src} alt={menuItem.name} />
       </a>
       <p>{menuItem.name}</p>
