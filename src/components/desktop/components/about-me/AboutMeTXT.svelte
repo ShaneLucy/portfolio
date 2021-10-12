fb814ecb854bddf5700a904a3a50770a99091f53
Shane Lucy <shanelucy01@protonmail.com>
2021-10-12 19:50:59 +0100

    Created each about me variant

---
+++ src/components/desktop/components/about-me/AboutMeTXT.svelte
@@ -0,0 +1,19 @@
+<div>
+  <div>
+    <pre>This is a pre formatted block, i need to add a some content here</pre>
+  </div>
+</div>
+
+<style>
+  div {
+    width: 100%;
+    height: 100%;
+    background-color: var(--background-tertiary);
+  }
+
+  div div {
+    width: calc(100% - 1.5rem);
+    height: calc(100% - 1.5rem);
+    margin: 0.75rem;
+  }
+</style>
