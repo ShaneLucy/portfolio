96452e8bfc98e6e06bc3fe6ab434027fe7680a1e
Shane Lucy <shanelucy01@protonmail.com>
2021-12-12 16:12:25 +0000

    Removed file

--- src/helpers/index.ts
+++
@@ -1,9 +0,0 @@
-import type { Dialog } from '../types';
-
-/**
- * Returns the index of the active dialog
- */
-export default (dialog: Array<Dialog>): number => {
-  const activeIndex = dialog.findIndex((x) => x.active);
-  return activeIndex;
-};
