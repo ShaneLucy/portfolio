1fb460668b6e919d7507f462ac0f8a44ffa36c1b
Shane Lucy <shanelucy01@protonmail.com>
2021-12-12 16:16:07 +0000

    Updated Dialog interface

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,8 @@
-import type FileExplorer from '../components/desktop/components/dialog/FileExplorer.svelte';
+import FileExplorer from '../components/desktop/components/FileExplorer.svelte';
 import type About from '../components/desktop/components/About.svelte';
 import type Projects from '../components/desktop/components/Projects.svelte';
 import type Home from '../components/desktop/components/Home.svelte';
+import type AboutMeContent from '../components/desktop/components/dialog/AboutMeTXT.svelte';
 
 export interface FileExplorerMenu {
   name: string;
@@ -10,10 +11,12 @@ export interface FileExplorerMenu {
 }
 
 export interface Dialog {
-  component: typeof FileExplorer;
+  component: typeof FileExplorer | AboutMeContent;
   openingActiveTab: number;
   display: 'min' | 'default' | 'max';
-  fileExplorerState: Array<FileExplorerMenu>;
-  isActive: boolean;
-  isOpen: true;
+  fileExplorerState: Array<FileExplorerMenu> | [];
+  active: boolean;
+  open: boolean;
+  title: string;
+  id: number;
 }
